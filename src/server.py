9a2df0f8c3db6997560e4f59e5832dc7f8968f90
Diana Drugea <drugeadiana@gmail.com>
2024-12-12 15:28:45 +0200

    Working on the cities. Maybe done

--- src/server.py
+++ src/server.py
@@ -2,12 +2,6 @@ from flask import Flask, Response, jsonify, request
 import psycopg2
 
 app = Flask(__name__)
-countries_list = [] # debuging for now
-country_counter = 0
-cities_list = [] # debuging for now
-cities_counter = 0
-temperature_list = [] # debuging for now
-temperature_counter = 0
 
 hostname = "db"
 port = 5432
@@ -17,6 +11,7 @@ password = "pass"
 
 db_conn = psycopg2.connect(host=hostname, port=port, dbname=db_name, user=username, password=password)
 db_cursor = db_conn.cursor()
+
 # Countries
 
 def record_into_json_country():
@@ -34,7 +29,8 @@ def record_into_json_country():
 
 @app.route("/api/countries", methods=["POST"])
 def post_countries():
-    global countries_list, country_counter
+    global country_counter, db_cursor, country_keys, country_types
+
     data = request.get_json()
     if not data:
         return Response(status=400)
@@ -43,7 +39,7 @@ def post_countries():
     if not ((isinstance(data["nume"], str)) and (isinstance(data["lat"], float)) and (isinstance(data["lon"], float))):
         return Response(status=400)
 
-    new_country = {"id" : country_counter, "name" : data["nume"], "lat" : data["lat"], "long" : data["lon"]}
+    new_country = {"name" : data["nume"], "lat" : data["lat"], "long" : data["lon"]}
     
     # Verificam daca tara deja exista in db
     db_cursor.execute("SELECT * FROM TARI WHERE nume_tara = %s", (new_country["name"],))
@@ -52,15 +48,14 @@ def post_countries():
 
     # Verificam in cazul in care exista deja cheia in db
     try:
-        db_cursor.execute("INSERT INTO TARI (id_tara, nume_tara, latitudine, longitudine) VALUES (%s, %s, %s, %s)", (new_country["id"], new_country["name"], new_country["lat"], new_country["long"]))
+        db_cursor.execute("INSERT INTO TARI (nume_tara, latitudine, longitudine) VALUES (%s, %s, %s)", (new_country["name"], new_country["lat"], new_country["long"]))
         db_cursor.execute("COMMIT")
-    except psycopg2.errors.UniqueViolation:
+    except:
         db_conn.rollback()
         return Response(status=409)
 
-    # Selectam id-ul, crestem counter-ul.
+    # Selectam id-ul tarii adaugate
     db_cursor.execute("SELECT id_tara FROM TARI WHERE nume_tara = %s", (new_country["name"],))
-    country_counter = country_counter + 1
     response = {"id": db_cursor.fetchone()[0]}
     return jsonify(response), 201
 
@@ -73,12 +68,11 @@ def get_countries():
 
 @app.route("/api/countries/<int:id>", methods=["PUT"])
 def put_country(id):
-    global db_cursor
+    global db_cursor, country_keys, country_types
     data = request.get_json()
-
     if not data:
         return Response(status=400)
-    if {"nume", "lat", "lon", "id"} != data.keys():
+    if {"nume", "lat", "lon"} != data.keys():
         return Response(status=400)
     if not ((isinstance(data["nume"], str)) and (isinstance(data["lat"], float)) and (isinstance(data["lon"], float))):
         return Response(status=400)
@@ -97,7 +91,7 @@ def put_country(id):
 @app.route("/api/countries/<int:id>", methods=["DELETE"])
 def delete_country(id):
     global db_cursor
-    
+
     query = "SELECT * FROM TARI WHERE id_tara = CAST(%s as INT)" % id
     db_cursor.execute(query)
     response = db_cursor.fetchone()
@@ -108,6 +102,111 @@ def delete_country(id):
     else:
         return Response(status=404)
 
+# Cities
+
+def record_into_json_city():
+    global db_cursor
+    rows = db_cursor.fetchall()
+    records = []
+
+    for row in rows:
+        record = {}
+        record["id"] = row[0]
+        record["idTara"] = row[1]
+        record["nume"] = row[2]
+        record["lat"] = row[3]
+        record["lon"] = row[4]
+        records.append(record)
+
+    return records
+
+@app.route("/api/cities", methods=["POST"])
+def post_cities():
+    global db_cursor, city_keys, city_types
+
+    data = request.get_json()
+    if not data:
+        return Response(status=400)
+    if {"idTara","nume", "lat", "lon"} != data.keys():
+        return Response(status=400)
+    if data["nume"] == "" or data["nume"] is None or data["lat"] == "" or data["lat"] is None or data["lon"] == "" or data["lon"] is None or data["idTara"] == "" or data["idTara"] is None:
+        return Response(status=400)
+
+    # Verificam daca tara deja exista in db
+    db_cursor.execute("SELECT * FROM ORASE WHERE nume_oras = %s", (data["nume"],))
+    if db_cursor.fetchone():
+        return Response(status=409)
+
+    # Verificam in cazul in care exista deja cheia in db
+    try:
+        db_cursor.execute("INSERT INTO ORASE (id_tara, nume_oras, latitudine, longitudine) VALUES (%s, %s, %s, %s)", (data["idTara"], data["nume"], data["lat"], data["lon"]))
+        db_cursor.execute("COMMIT")
+    except:
+        db_conn.rollback()
+        return Response(status=409)
+
+    # Selectam id-ul tarii adaugate
+    db_cursor.execute("SELECT id_tara FROM ORASE WHERE nume_oras = %s", (data["nume"],))
+    response = {"id": db_cursor.fetchone()[0]}
+    return jsonify(response), 201
+    
+@app.route("/api/cities", methods=["GET"])
+def get_cities():
+    global db_cursor
+    db_cursor.execute("SELECT * FROM ORASE")
+    return jsonify(record_into_json_city()), 200
+
+@app.route("/api/cities/country/<int:id>", methods=["GET"])
+def get_citites_in_country(id):
+    global db_cursor
+    query = "SELECT * FROM ORASE WHERE id_tara = CAST(%s as INT)" % id
+    db_cursor.execute(query)
+    return jsonify(record_into_json_city()), 200
+
+@app.route("/api/cities/<int:id>", methods=["PUT"])
+def put_city(id):
+    global db_cursor
+
+    data = request.get_json()
+    if not data:
+        return Response(status=400)
+    if {"id", "idTara","nume", "lat", "lon"} != data.keys():
+        return Response(status=400)
+    if data["nume"] == "" or data["nume"] is None or data["lat"] == "" or data["lat"] is None or data["lon"] == "" or data["lon"] is None or data["idTara"] == "" or data["idTara"] is None:
+        return Response(status=400)
+    if id != data["id"]:
+        return Response(status=400)
+    
+    query = "SELECT * FROM ORASE WHERE id_oras = CAST(%s as INT)" % id
+    db_cursor.execute(query)
+    response = db_cursor.fetchone()
+    if response:
+        try:
+            db_cursor.execute("UPDATE ORASE SET id_tara = %s, nume_oras = %s, latitudine = %s, longitudine = %s WHERE id_oras = %s", (data["idTara"], data["nume"], data["lat"], data["lon"], id))
+            db_cursor.execute("COMMIT")
+            return Response(status=200)
+        except:
+            db_conn.rollback()
+            return Response(status=409)
+    else:
+        return Response(status=404)
+    
+
+@app.route("/api/cities/<int:id>", methods=["DELETE"])
+def delete_city(id):
+    global db_cursor
+    query = "SELECT * FROM ORASE WHERE id_oras = CAST(%s as INT)" % id
+    db_cursor.execute(query)
+    response = db_cursor.fetchone()
+    if response:
+        db_cursor.execute("DELETE FROM ORASE WHERE id_oras = %s", (id,))
+        db_cursor.execute("COMMIT")
+        return Response(status=200)
+    else:
+        return Response(status=404)
+
+
+    
 
 if __name__ == "__main__":
     app.run('0.0.0.0', debug=True)
